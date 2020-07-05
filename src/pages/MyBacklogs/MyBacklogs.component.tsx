cf8d49709c18066c26b1aa36c3d7ffa12a7ff3ef
Joshua Grant <grantjoshua1995@gmail.com>
2020-07-05 19:04:26 +0100

    Adding debounce function and input to test out igdb call

--- src/pages/MyBacklogs/MyBacklogs.component.tsx
+++ src/pages/MyBacklogs/MyBacklogs.component.tsx
@@ -2,19 +2,38 @@ import React, { useEffect, useState, useCallback } from 'react';
 import api from '../../../services/api';
 import { Backlog as BacklogType } from '../../../types';
 import { Backlog } from '../../components/Backlog';
+import useDebounce from '../../../services/debounce';
 
 export interface MyBacklogsProps {}
 
 export const MyBacklogs: React.FC<MyBacklogsProps> = () => {
   const [backlogs, setBacklogs] = useState<BacklogType[]>([]);
-  const [newBacklogFocus, setNewBacklogFocus] = useState<boolean>(false);
+  const [gameSearch, setGameSearch] = useState<string>('');
+  const [gameList, setGameList] = useState<[]>([]);
+
+  const debouncedSearchTerm = useDebounce(gameSearch, 250);
 
   const getUserBacklogs = useCallback(async () => {
     const userBacklogs = await api.getUserBacklogs(1);
-    const igdbGames = await api.getIgdbGames('Halo');
-    console.log(igdbGames);
+
     setBacklogs(userBacklogs);
   }, [backlogs]);
+
+  const updateGameSearch = useCallback(event => {
+    setGameList([]);
+    setGameSearch(event.target.value);
+  }, []);
+
+  const getIgdbGames = useCallback(async () => {
+    const igdbGames = await api.getIgdbGames(gameSearch);
+    setGameList(igdbGames);
+  }, [gameSearch]);
+
+  useEffect(() => {
+    if (debouncedSearchTerm.length >= 3) {
+      getIgdbGames();
+    }
+  }, [debouncedSearchTerm]);
 
   useEffect(() => {
     document.title = 'Backlogify - My Backlogs';
@@ -33,6 +52,15 @@ export const MyBacklogs: React.FC<MyBacklogsProps> = () => {
             <span>Add another backlog...</span>
           </a>
         </div>
+      )}
+      {/* Placeholder input just to prove searching for games works */}
+      <input placeholder='Search here!' value={gameSearch} onChange={updateGameSearch} />
+      {gameList.length > 0 && (
+        <ul>
+          {gameList.map(game => {
+            return <li>{game.name}</li>;
+          })}
+        </ul>
       )}
     </div>
   );
